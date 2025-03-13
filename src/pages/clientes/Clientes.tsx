ce08177a52a1b3416bee8367e3d3de7ca34b59b9
FelipeR96 <felipereinaleso96@gmail.com>
2025-03-13 14:46:48 -0500

    Create Clientes.tsx
    Se crea nueva page para clientes

---
+++ src/pages/clientes/Clientes.tsx
@@ -0,0 +1,13 @@
+import CreateClientes from "../../components/rentasyregistro/CreateClientes"
+import { BaseLayout } from "../../layout/Layout"
+
+
+const Clientes = () => {
+  return (
+    <BaseLayout tituloPagina="CLIENTES">
+        <CreateClientes/>
+    </BaseLayout>
+  )
+}
+
+export default Clientes
